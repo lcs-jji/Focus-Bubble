573e1e7ac327ddbb704f3a8c68534fb4eb278fb0
Joseph Ji <jji@lcs.on.ca>
2025-04-30 20:08:04 -0400

    Initial Commit

---
+++ Focus Bubble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Focus Bubble
+//
+//  Created by Xinchen Ji on 2025-04-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Focus Bubble/Focus_BubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Focus_BubbleApp.swift
+//  Focus Bubble
+//
+//  Created by Xinchen Ji on 2025-04-30.
+//
+
+import SwiftUI
+
+@main
+struct Focus_BubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
